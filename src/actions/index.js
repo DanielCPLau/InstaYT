66142709868aeb7cc6f93f13df46b7be77984e37
Daniel Lau <danielcplau@gmail.com>
2016-11-12 23:31:54 -0800

    Fetch from youtube API and updated reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,13 @@
+import 'whatwg-fetch';
+
+var key = 'key=AIzaSyBOryAXJUTN31wmfvIWTQyt1UbN3-e-TdA';
+
 export function setVideo(term) {
 	console.log("setVideo called");
 	return {
 		type: 'SET_VIDEO',
 		payload: term
 	};
-}
-
-export function onSearchTermChange(term) {
-	console.log("onSearchTermChange called");
-	return {
-		type: 'TERM_CHANGE',
-		payload: term
-	}
 }
 
 export function onVideoSelect(video) {
@@ -21,3 +17,37 @@ export function onVideoSelect(video) {
 		payload: video
 	}
 }
+
+//async thunk call
+export function fetchVideos(term) {
+	console.log("FETCHING VIDS");
+	return function (dispatch) {
+		dispatch(requestVideos(term))
+
+		return fetch('https://www.googleapis.com/youtube/v3/search?part=snippet&' + key + '&type=video&maxResults=16&q=' + term)
+		.then(response => response.json())
+		.then(json =>
+
+			dispatch(receiveVideos(term, json))
+		)
+	}
+}
+
+export function requestVideos(term) {
+  return {
+    type: 'REQUEST_VIDEOS',
+    term
+  }
+}
+
+export function receiveVideos(term, json) {
+  return {
+    type: 'RECEIVE_VIDEOS',
+    term,
+    videos: json.items,
+  }
+}
+
+
+
+
--- src/reducers/reducer_video.js
+++ src/reducers/reducer_video.js
@@ -1,6 +1,7 @@
 import YTSearch from 'youtube-api-search';
 
 const initialState = {
+	isFetching: false,
 	selectedVideo: null,
 	videos: []
 }
@@ -8,34 +9,23 @@ const initialState = {
 export default function(state = initialState, action) {
 	switch(action.type) {
 	//calls to youtube API to search video
-		case 'TERM_CHANGE':
-			console.log("TERM_CHANGE called" + action.payload);
-			const API_KEY = 'AIzaSyBOryAXJUTN31wmfvIWTQyt1UbN3-e-TdA';
 
-			var APIvideos = 0;
-			var APIselectedVideo = 0;
+		case 'REQUEST_VIDEOS':
+			return {...state, 
+				isFetching: true }
 
-			var loading = true;
-
-			function getVideos() {
-
-				YTSearch({key: API_KEY, term: action.payload}, videos => {
-					console.log(videos)
-					this.APIvideos = videos;
-				})
-
-				console.log("DONE")
+		case 'RECEIVE_VIDEOS':
+			var fullVid = action.videos;
+			var firstVid = fullVid[0];
+			fullVid.shift();
+			return {...state,
+				videos: fullVid,
+				selectedVideo: firstVid
 			}
 
-			getVideos();
-			console.log(APIvideos)
-
-			return {
-				...state,
-				selectedVideo: APIselectedVideo,
-				videos: APIvideos
-			};
-
+		case 'SELECT_VIDEO':
+			return {...state,
+				selectedVideo: action.payload}
 		default:
 			return state;
 	}
